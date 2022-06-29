d7b90eed06e35dbd99d7746d370137a75ffc0c57
Randall <randalltr@proton.me>
2022-06-29 11:04:57 +0700

    complete URL shortener before bootstrap

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, abort
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session, jsonify
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -6,11 +6,13 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.secret_key = 'h23l4nvkl2390lknwe23ngfklwef'
 
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', codes=session.keys())
 
-@app.route('/your-url', methods=['GET','POST'])
+
+@app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
     if request.method == 'POST':
         urls = {}
@@ -24,19 +26,20 @@ def your_url():
             return redirect(url_for('home'))
 
         if 'url' in request.form.keys():
-            urls[request.form['code']] = {'url':request.form['url']}
+            urls[request.form['code']] = {'url': request.form['url']}
         else:
             f = request.files['file']
             full_name = request.form['code'] + secure_filename(f.filename)
             f.save('./static/user_files/' + full_name)
-            urls[request.form['code']] = {'file':full_name}
+            urls[request.form['code']] = {'file': full_name}
 
-        
-        with open('urls.json','w') as url_file:
+        with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
+            session[request.form['code']] = True
         return render_template('your_url.html', code=request.form['code'])
     else:
         return redirect(url_for('home'))
+
 
 @app.route('/<string:code>')
 def redirect_to_url(code):
@@ -51,6 +54,11 @@ def redirect_to_url(code):
 
     return abort(404)
 
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
+
+@app.route('/api')
+def session_api():
+    return jsonify(list(session.keys()))
